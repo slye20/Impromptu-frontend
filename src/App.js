0d991c9596277b3b89c0c6e9af063f1a46ffca16
slye20 <samuellim2011@gmail.com>
2024-05-27 20:46:44 +0800

    Test git

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h1 className="text-3xl font-bold underline">Hello world!</h1>
+        <h1 className="text-3xl font-bold underline">Hello WORLD!</h1>
       </header>
     </div>
   );
